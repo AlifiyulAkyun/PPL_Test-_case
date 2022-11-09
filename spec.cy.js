efafb2774ca8018701ffdb75de8419b77da176f0
rofiqohfifi28 <99854026+rofiqohfifi28@users.noreply.github.com>
2022-11-09 16:22:52 +0700

    rofiqoh wahyuningtyas

--- spec.cy.js
+++ spec.cy.js
@@ -1,8 +1,12 @@
+
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.vidio.com/users/login')
-    cy.get('#user_login').type('lutfiyahmahmudah11@gmail.com')
-    cy.get('#user_password').type('nadhifah2001')
-    cy.get('#onboarding-login-form-submit').click()
+  it('berhasil login', () => {
+    cy.visit('https://www.instagram.com/accounts/emailsignup/?hl=id')
+    cy.get(':nth-child(4) > .xnz67gz > ._aa48 > ._aa4b').type('gpolinema@gmail.com')
+    cy.get(':nth-child(5) > .xnz67gz > ._aa48 > ._aa4b').type('rofiqohwahyuningtyas')
+    cy.get(':nth-child(6) > .xnz67gz > ._aa48 > ._aa4b').type('fifirofiqoh')
+    cy.get(':nth-child(7) > .xnz67gz > ._aa48 > ._aa4b').type('fifi123')
+    cy.get(':nth-child(9) > ._ab8w')
+    
+  })  
   })
-})
